f5795483c3d9bba48b581694b73412f0933f8368
zhangxingsong <ex-zhangxingsong001>
2018-02-24 10:07:14 +0800

    Initial Commit

---
+++ Dcct/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Dcct
+//
+//  Created by 张兴松 on 2018/2/24.
+//  Copyright © 2018年 Dcct. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
